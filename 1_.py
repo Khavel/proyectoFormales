f7bef10b4e70986863801a288f01c72a19c49195
Khavel <Cejatr0n>
2018-05-16 01:28:22 +0200

    todo ok

--- 1_.py
+++ 1_.py
@@ -13,16 +13,16 @@ def aplicarProducciones(palabra):
 
 def aplicarRegla(simbolo):
     if simbolo == "f":
-        rand = random.randrange(1, 5)
-        if rand == 1.0:
+        rand = random.randrange(1, 10)
+        if rand in [1.0,2.0,3.0,4.0]:
             return "*f[+f[++f-f][-f+f]][-f[-f+f][+f-f]]ff"
-        elif rand == 2.0:
+        elif rand in [5.0]:
             return "+*f[+f[++f-f][-f+f]][-f]fff"
-        elif rand == 3.0:
+        elif rand in [6.0]:
             return "fff--f[++f-f]/"
-        elif rand == 4.0:
+        elif rand in [7.0,8.0]:
             return "*f[+f[-f+f]][+f-f]fff"
-        elif rand == 5.0:
+        elif rand in [9.0,10.0]:
             return "*f[++f-f]++"
     else:
         return simbolo
@@ -30,16 +30,16 @@ def aplicarRegla(simbolo):
 
 
 def makePoly(cadena):
-    step = 0.5
-    stepV = 0.4
+    step = random.uniform(0.3,0.8)
+    stepV = random.uniform(0.2,0.6)
     orientacion = [step,step,stepV]
     pos = [0,0,0]
     brackets = []
     ant = 0
     vertCont = 0
     trasBracket = False
-    posPre = []
-    cAnt = 'x'
+    posPre = [] #Pila en la que se guarda la posicion cada vez que se hace una ramificacion
+    cAnt = 'x' #Variable para guardar el caracter anterior en el bucle
 
     for c in cadena:
 
@@ -54,6 +54,7 @@ def makePoly(cadena):
                 else:
                     trasBracket = False
                     edges.append((vertCont-1,ant))
+                    hojas.append(vertCont-2)
 
         #Guarda la posicion
         elif c == '[':
@@ -194,8 +195,9 @@ edges = []
 faces = []
 vverts = []
 raices = []
+hojas = []
 
-vueltas = 5
+vueltas = 3
 palabra = "f"
 for i in range(vueltas):
     palabra = palabra + aplicarProducciones(palabra)
@@ -220,15 +222,28 @@ scn.objects.link(obj)
 
 bpy.context.scene.objects.active = obj
 
-#bpy.ops.object.mode_set( mode = 'OBJECT' )
-#obj.modifiers.new("subd", type='SUBSURF')
-#obj.modifiers['subd'].levels = 1
+bpy.ops.object.mode_set( mode = 'OBJECT' )
+obj.modifiers.new("subd", type='SUBSURF')
+obj.modifiers['subd'].levels = 1
 bpy.ops.object.modifier_apply(modifier="subd")
 obj.modifiers.new("skin", type='SKIN')
 obj.modifiers.new("subd", type='SUBSURF')
 obj.modifiers['subd'].levels = 1
+mat = bpy.data.materials.new(name="Marron") #set new material to variable
+obj.data.materials.append(mat) #add the material to the object
+bpy.context.object.active_material.diffuse_color = (0.118,0.060,0.040) #change color
 grosores = []
-grosor = 0.5
+grosor = 0.05
+for i,v in enumerate(obj.data.skin_vertices[0].data):
+    v.radius = random.uniform(0.05,0.2), random.uniform(0.05,0.2)
+    if i in hojas:
+        bpy.ops.mesh.primitive_ico_sphere_add(location=verts[i],subdivisions=2,size=random.uniform(0.2,0.8))
+        ob = bpy.context.active_object
+        mat = bpy.data.materials.new(name="Verde") #set new material to variable
+        ob.data.materials.append(mat) #add the material to the object
+        bpy.context.object.active_material.diffuse_color = (0, random.uniform(0.6,1), random.uniform(0.2,0.8)) #change color
+
+'''
 for i,v in enumerate(obj.data.skin_vertices[0].data):
     vActual = verts[i]
     vSiguiente = verts[i+1]
@@ -240,3 +255,4 @@ for i,v in enumerate(obj.data.skin_vertices[0].data):
 
     v.radius = grosor, grosor
     grosor = grosor - 0.5/len(verts)
+'''
